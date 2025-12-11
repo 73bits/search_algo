c735cd7cb52f38561a826f5f47d7b9aa822ec7e1
73bits <prashantkr0628@gmail.com>
2025-12-11 19:23:09 +0530

    inital commit

---
+++ 01_linear_search/05_lc_max_wealth/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+	public static void main(String ... args) {
+		int[][] accounts = new int[][] {
+			{2, 3, 4},
+			{1, 50, 4},
+			{9, 13, 4}
+		};
+
+		System.out.println(maximumWealth(accounts));
+	}
+
+	public static int maximumWealth(int[][] accounts) {
+		int maxWealth = Integer.MIN_VALUE;
+		for (int customer = 0; customer < accounts.length; customer++) {
+			int total = 0;
+			for (int account = 0; account < accounts[customer].length; account++) {
+				total = total + accounts[customer][account];
+			}
+			if (total > maxWealth) {
+				maxWealth = total;
+			}
+		}
+		return maxWealth;
+	}
+}
