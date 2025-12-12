c5a86851e398ecc8a13ba6cf6e36ed78a134081b
73bits <prashantkr0628@gmail.com>
2025-12-13 00:41:13 +0530

    initial commit

---
+++ 02_binary_search/00_main/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String ... args) {
+		int[] arr = new int[] {-1, 2, 5, 8, 9, 16, 24, 98, 112, 456};
+		int target = 112;
+		System.out.println("target index: " + search(arr, target));
+	}
+
+	private static int search(int[] arr, int target) {
+		int start = 0;
+		int end = arr.length;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] == target) return mid;
+			if (arr[mid] > target) end = mid - 1;
+			if (arr[mid] < target) start = mid + 1;
+		}
+		return -1;
+	}
+}
