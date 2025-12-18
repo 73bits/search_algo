32fc7950bdcef331c844366684e11bf6925f2864
73bits <prashantkr0628@gmail.com>
2025-12-18 19:10:48 +0530

    added index(): will find first & last index if target is found

--- binary_search/FirstLastLC.java
+++ binary_search/FirstLastLC.java
@@ -10,4 +10,22 @@ public class FirstLastLC {
 
 	private static int[] firstAndLastPosition(int[] nums, int target) {
 	}
+
+	// if target is found
+	// and if startIndex is true this will find first index of target (on left)
+	// and if startIndex is false this will find last index of target (on right)
+	private static int index(int[] nums, int target, boolean startIndex) {
+		int start = 0, end = nums.length - 1, ans = -1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (nums[mid] > target) end = mid - 1;
+			else if (nums[mid] < target) start = mid + 1;
+			else {
+				ans = mid;
+				if (startIndex) end = mid - 1;
+				else start = mid + 1;
+			}
+		}
+		return ans;
+	}
 }
