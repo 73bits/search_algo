84af871e2d8147795c254a736c5f6c477bba85a0
73bits <prashantkr0628@gmail.com>
2025-12-19 22:57:53 +0530

    checking only > and < conditions, otherwise return mid

--- binary_search/BinarySearch.java
+++ binary_search/BinarySearch.java
@@ -11,9 +11,9 @@ public class BinarySearch {
 		int end = arr.length - 1;
 		while (start < end) {
 			int mid = (start + end) / 2;
-			if (arr[mid] == target) return mid;
 			if (arr[mid] > target) end = mid - 1;
-			if (arr[mid] < target) start = mid + 1;
+			else if (arr[mid] < target) start = mid + 1;
+			else return mid;
 		}
 		return -1;
 	}
